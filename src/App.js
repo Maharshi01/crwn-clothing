fbe0be809b724e44bd20e090d5362f330c71561a
Maharshi01 <msganji1998@gmail.com>
2020-02-28 14:25:10 +0530

    Created Custom Components for FormInput and Button

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 import {Switch,Route} from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
-      
       <Route exact path='/' component={HomePage} ></Route>
-      <Route exact path='/hats' component={HatsPage} ></Route>
-
+      <Route  path='/shop' component={ShopPage} ></Route>
+      <Route  path='/signin' component={SignInAndSignUp} ></Route>
       </Switch>
-   
     </div>
   );
 }
